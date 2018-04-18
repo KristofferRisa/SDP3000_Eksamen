b14dad96966a72dda90a97259adaeb871b146e2a
WorshipTheSofa <jonas.jore@gmail.com>
2018-04-18 15:01:46 +0200

    blahblah

--- src/test.java
+++ src/test.java
@@ -1,7 +1,10 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		PDDocument doc = new PDDocument();
+		System.out.println(doc);
 	}
 
 }
