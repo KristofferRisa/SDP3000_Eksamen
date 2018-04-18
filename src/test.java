b4b1bcf064036248f6ea1132c8428bdae314cff0
WorshipTheSofa <jonas.jore@gmail.com>
2018-04-18 13:17:33 +0200

    pdfbox

--- src/test.java
+++ src/test.java
@@ -1,10 +1,7 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PDDocument doc = new PDDocument();
+		System.out.println("hello");
 	}
 
 }
